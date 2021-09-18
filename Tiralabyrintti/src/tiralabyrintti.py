65fe2d1087f7ef264c56536f65c57a78d599e163
JuhahuJ <lehtinen05@gmail.com>
2021-09-18 20:24:23 +0300

    projektintekotavan muutos

--- Tiralabyrintti/src/tiralabyrintti.py
+++
@@ -1,9 +0,0 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__ = "juha"
-__date__ = "$Sep 11, 2021 3:47:36 PM$"
-
-if __name__ == "__main__":
-    print "Hello World"
