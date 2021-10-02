5bfb020ef2f5f0f9254559da91f103763f57c4eb
JuhahuJ <juha.lehtinen@helsinki.fi>
2021-10-02 19:48:03 +0300

    ongelmia kuvien kanssa

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from solu import solu
 from ruudukko import ruudukko
 from random import randrange
-from tkinter import Tk, ttk
+from tkinter import *
 import tkinter
+from PIL import Image, ImageTk
 
 root = Tk()
-root.title("TkInter example")
 koko = 5
 ruutus = ruudukko(koko,root)
 ruutus.luo()
+canvas = tkinter.Canvas(root)
+canvas.pack
+
+root.photo = photo = tkinter.PhotoImage(file="y.gif")
+root.photo2 = photo2 = tkinter.PhotoImage(file="v.gif")
 
 tamanhetkinensolu = None
 lista = []
@@ -19,7 +24,6 @@ lista.append(aloitusruutu)
 while len(lista) > 0:
     viereiset = []
     nykyinensolu = lista[-1]
-    tkinter.Label(root, text="käyty", borderwidth=1 ).grid(row=nykyinensolu.y,column=nykyinensolu.x)
     lista.pop()
     if nykyinensolu.y == 0:
         pass
@@ -43,6 +47,12 @@ while len(lista) > 0:
         seuraavasolu.oltujo = True
         lista.append(seuraavasolu)
         print(seuraavasolu.koordinaatit())
-    root.update()
+
+kuva = tkinter.Label(root, image=photo, borderwidth=1 )
+kuva.grid(row=0,column=0)
+kuva2 = tkinter.Label(root, image=photo2, borderwidth=1 )
+kuva2.grid(row=0,column=0)
+canvas.create_image((0, 0), image=photo)
+canvas.create_image((0, 0), image=photo2)
 
 root.mainloop()
