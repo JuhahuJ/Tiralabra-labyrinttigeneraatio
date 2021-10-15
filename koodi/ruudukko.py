687286482d07b38e90d2b4d830babbde96dc4891
JuhahuJ <juha.lehtinen@helsinki.fi>
2021-10-15 10:03:13 +0300

    kommentit ruudukkoon ja soluun

--- koodi/ruudukko.py
+++ koodi/ruudukko.py
@@ -2,16 +2,30 @@ from solu import Solu
 
 
 class Ruudukko:
+    '''Ruudukko olio, jolle annetaan koko.'''
+
     def __init__(self, koko: int, ruudut=[]):
         self.koko = koko
         self.ruudut = ruudut
 
     def luo(self):
+        '''Täyttää ruudut soluilla perustuen annettuun kokoon, jolloin ruudukossa on koko*koko solua.
+
+        Ensimmäiseksi tyhjentää ruudut.
+
+        Silmukassa: Lisää ruutuihin väliaikaisen arvon, joka korvataan myöhemmin ja luo tyhjän apulistan.
+
+        Sisemmässä silmukassa: Luo solun koordinaateilla i, j, missä i on y ja j on x ja koordinaatit kasvavat ylhäältä alas, vasemmalta oikealle,
+        lisää solun apulistaan.
+
+        Ylemmässä silmukassa: korvaa väliaikaisen arvon apulistalla.
+        '''
+        
         self.ruudut = []
         for i in range(self.koko):
             self.ruudut.append("väliaikainen arvo")
             apulista = []
             for j in range(self.koko):
-                solus = Solu(i, j)
-                apulista.append(solus)
+                solu = Solu(i, j)
+                apulista.append(solu)
             self.ruudut[i] = apulista
--- koodi/solu.py
+++ koodi/solu.py
@@ -1,8 +1,12 @@
 class Solu:
+    '''Solu olio, joka saa arvot x, y ja oltujo'''
+
     def __init__(self, y, x, oltujo=False):
         self.x = x
         self.y = y
         self.oltujo = oltujo
 
     def koordinaatit(self):
+        '''Palauttaa solun koordinaatit'''
+        
         return (self.y, self.x)
