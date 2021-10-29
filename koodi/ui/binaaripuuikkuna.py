ea285a05cb10d3742e3ab8043f7692dca79e306e
JuhahuJ <juha.lehtinen@helsinki.fi>
2021-10-29 09:00:55 +0300

    binääripuun läpikäynti toimii

--- koodi/ui/binaaripuuikkuna.py
+++ koodi/ui/binaaripuuikkuna.py
@@ -22,12 +22,32 @@ class BinaaripuuIkkuna:
         self._frame.destroy()
 
     def nayta_miten_luotu(self, canvas, lapikaynti):
-        canvas.create_rectangle(lapikaynti[0].x*20+7, lapikaynti[0].y*20+7, lapikaynti[0].x*20+23, lapikaynti[0].y*20+23,fill="green")
-        for solu in range(len(lapikaynti)):
-            canvas.create_rectangle(lapikaynti[solu].x*20+12, lapikaynti[solu].y*20+12, lapikaynti[solu].x*20+18, lapikaynti[solu].y*20+18,fill="blue")
-            canvas.create_rectangle(lapikaynti[solu-1].x*20+10, lapikaynti[solu-1].y*20+10, lapikaynti[solu-1].x*20+20, lapikaynti[solu-1].y*20+20,fill="red")
+        paikka = 0
+
+        for i in range(self.koko+1):
+            canvas.create_line(paikka*20+5, 5, paikka*20+5, 20*self.koko+5)
+            paikka += 1
+        paikka = 0
+        for j in range(self.koko+1):
+            canvas.create_line(5, paikka*20+5, 20*self.koko+5, paikka*20+5)
+            paikka += 1
+        for solu in lapikaynti:
+            if solu[1] == "alas" and solu[0].y < self.koko-1:
+                canvas.create_line(solu[0].x*20+6, solu[0].y*20+25,
+                                        solu[0].x*20+25, solu[0].y*20+25, fill="white")
+            elif solu[1] == "alas" and solu[0].x < self.koko-1:
+                canvas.create_line(solu[0].x*20+25, solu[0].y*20+6,
+                                        solu[0].x*20+25, solu[0].y*20+25, fill="white")
+            elif solu[1] == "oikealle" and solu[0].x < self.koko-1:
+                canvas.create_line(solu[0].x*20+25, solu[0].y*20+6,
+                                        solu[0].x*20+25, solu[0].y*20+25, fill="white")
+            elif solu[1] == "oikealle" and solu[0].y < self.koko-1:
+                canvas.create_line(solu[0].x*20+6, solu[0].y*20+25,
+                                        solu[0].x*20+25, solu[0].y*20+25, fill="white")
+            canvas.create_rectangle(solu[0].x*20+12, solu[0].y*20+12, solu[0].x*20+18, solu[0].y*20+18,fill="blue")
             self._root.update()
             sleep(0.1)
+            canvas.create_rectangle(solu[0].x*20+10, solu[0].y*20+10, solu[0].x*20+20, solu[0].y*20+20,fill="white", outline="")
 
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
@@ -51,8 +71,8 @@ class BinaaripuuIkkuna:
 
         for i in range(len(ruudukko.ruudut)):
             for solu in ruudukko.ruudut[i]:
-                lapikaynti.append(solu)
                 suunta = choice(["alas","oikealle"])
+                lapikaynti.append([solu,suunta])
                 if suunta == "alas" and solu.y < self.koko-1:
                     canvas.create_line(solu.x*20+6, solu.y*20+25,
                                            solu.x*20+25, solu.y*20+25, fill="white")
